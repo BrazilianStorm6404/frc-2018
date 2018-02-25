cd50655f585520a39fef7ab4a6b75e8bd701be1f
BrazilianStorm6404 <36820183+BrazilianStorm6404@users.noreply.github.com>
2018-02-25 17:38:03 -0300

    Nomes de variaveis mais legiveis

--- src/org/usfirst/frc/team6404/robot/Controle.java
+++ src/org/usfirst/frc/team6404/robot/Controle.java
@@ -12,28 +12,28 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class Controle {
-	private PWMSpeedController m1,m2,m3,m4, m5, m6 ,m7, m8;
-	private SpeedControllerGroup m12;
-	private SpeedControllerGroup m34;
+	private PWMSpeedController tracao1,tracao2,tracao3,tracao4, subidaEsq, subidaDir ,roleteEsq, roleteDir;
+	private SpeedControllerGroup motoresFrontais;
+	private SpeedControllerGroup motoresTraseiros;
 	private DigitalInput limit1;
 	protected Joystick controller;
 	private DifferentialDrive drive;
 	boolean mode;
 	
 	public Controle() {
-		this.m1 = new Spark(0);
-		this.m2 = new Spark(1);
-		this.m3 = new Spark(2);
-		this.m4 = new Spark(3);
-		this.m12= new SpeedControllerGroup(m1,m2);
-		this.m34= new SpeedControllerGroup(m3,m4);
-		this.drive= new DifferentialDrive(m12,m34);
+		this.tracao1 = new Spark(0);
+		this.tracao2 = new Spark(1);
+		this.tracao3 = new Spark(2);
+		this.tracao4 = new Spark(3);
+		this.motoresFrontais= new SpeedControllerGroup(tracao1,tracao2);
+		this.motoresTraseiros= new SpeedControllerGroup(tracao3,tracao4);
+		this.drive= new DifferentialDrive(this.motoresFrontais,this.motoresTraseiros);
 		this.limit1 = new DigitalInput(7);
 		this.controller = new Joystick(0);
-		this.m5 = new Spark(4);
-		this.m7 = new VictorSP(7);
-		this.m8 = new Jaguar(8);
-		this.m6 = new Spark(5);
+		this.subidaEsq = new Spark(4);
+		this.subidaDir = new Spark(5);
+		this.roleteDir = new VictorSP(7);
+		this.roleteEsq = new Jaguar(8);
 	}
 	public void tracao() {
 		drive.arcadeDrive(controller.getY(), controller.getX());
@@ -42,15 +42,15 @@ public class Controle {
 	public void botaoLTeRT() {
 
 		if (controller.getRawAxis(2) > 0.2) {
-			m5.set(controller.getRawAxis(2));
-			m6.set(-controller.getRawAxis(2) * 0.965);
+			subidaEsq.set(controller.getRawAxis(2));
+			subidaDir.set(-controller.getRawAxis(2) * 0.965);
 		} else if (controller.getRawAxis(3) > 0.2) {
-			m5.set(-controller.getRawAxis(3));
-			m6.set(controller.getRawAxis(3) * 0.965);
+			subidaEsq.set(-controller.getRawAxis(3));
+			subidaDir.set(controller.getRawAxis(3) * 0.965);
 		} else {
 			
-			m5.set(0);
-			m6.set(0);
+			subidaEsq.set(0);
+			subidaDir.set(0);
 		}
 	}
 
@@ -72,14 +72,14 @@ public class Controle {
 	
 	public void botaoLBeRB() {
 		if (controller.getRawButton(5) /*&& !limit1.get()*/) {
-			m7.set(0.7);
-			m8.set(-0.7);
+			roleteEsq.set(0.7);
+			roleteDir.set(-0.7);
 		} else if (controller.getRawButton(6)) {
-			m7.set(-0.7);
-			m8.set(0.7);
+			roleteEsq.set(-0.7);
+			roleteDir.set(0.7);
 		} else {
-			m7.set(0);
-			m8.set(0);
+			roleteEsq.set(0);
+			roleteDir.set(0);
 		}
 	}
 }
